2520b0cc1b18f25e0bc73fdf1d1a3f0e08b8646a
Rexxboy02 <74881263+Rexxboy02@users.noreply.github.com>
2023-03-03 21:30:54 +0100

    Update UserInput_ElevatorConversor.py

--- UserInput_ElevatorConversor.py
+++ UserInput_ElevatorConversor.py
@@ -1,8 +1,3 @@
-#var1 nos pide un input del usuario, entregandole a él la
-#información de que debe introducir el 'Piso del ascensor en Europa'
-var1 = input ('Piso del ascensor en Europa?')
-#nombramos la variable que nos entregará el valor convertido a pisos EEUU,
-#convertimos el valor de la variable con input a un integer para poder sumarle
+var1 = input ('Elevator height in Europe?')
 converted = int(var1) + 1
-#escribimos los resultados, separando el texto del valor con una coma (,)
-print ("Piso del ascensor en EEUU", converted)
+print ("Elevator height in USA", converted)
